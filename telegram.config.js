61cb184b6438bc1b9a27c024029be095d59eb722
atksoa <senxinfo@gmail.com>
2026-08-01 15:38:05 +0300

    Update telegram.config.js

--- telegram.config.js
+++ telegram.config.js
@@ -1,6 +1,6 @@
 // telegram.config.js
 module.exports = {
     botUsername: 'MenaceAuthRobot',   // Ваш бот
-    botToken: '8712904392:AAG8rMxiThfW-iFUpzDQ2EzauAL1CNlpKJE',
+    botToken: '8712904392:AAGMl7OBly82D2zD7Wt2vyfjqSg4B4nfio8',
     authPageUrl: 'https://flamehost1k.github.io/dsdfsaksaokdkfdpasoasppdfoposadsdafdfsa/auth.html'
 };
